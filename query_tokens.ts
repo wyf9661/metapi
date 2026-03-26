0bab58fd1bc2e784b687676d4c8a5c0d149543c5
apple <apple@appledeMacBook-Pro.local>
2026-03-26 21:59:48 +0800

    chore: remove committed local debug artifacts

--- query_tokens.ts
+++
@@ -1,9 +0,0 @@
-import Database from 'better-sqlite3';
-import fs from 'fs';
-
-const dbPath = './data/hub.db';
-const db = new Database(dbPath, { readonly: true });
-
-const tokens = db.prepare(`SELECT * FROM account_tokens WHERE account_id = 18`).all();
-fs.writeFileSync('account_tokens_18.json', JSON.stringify(tokens, null, 2));
-console.log('Saved tokens to account_tokens_18.json', tokens.length);
