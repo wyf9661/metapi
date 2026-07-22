85d6c9a0b89ed0e298776eafb944754bb85b2973
wyf9661 <wyf9661@hotmail.com>
2026-07-22 14:19:09 +0800

    chore: sync generated schema artifacts

--- src/server/db/generated/mysql.upgrade.sql
+++ src/server/db/generated/mysql.upgrade.sql
@@ -1,1 +1,2 @@
--- no schema changes detected for mysql
+ALTER TABLE `proxy_logs` ADD COLUMN `request_trace_id` TEXT;
+CREATE INDEX `proxy_logs_request_trace_id_created_at_idx` ON `proxy_logs` (`request_trace_id`(191), `created_at`);
